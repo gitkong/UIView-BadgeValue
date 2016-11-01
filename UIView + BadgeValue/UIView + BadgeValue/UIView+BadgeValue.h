815d0357955e253b2bd76828782197b20ff59ca3
clarence <13751855378@163.com>
2016-11-01 12:56:57 +0800

    添加备注

--- UIView + BadgeValue/UIView + BadgeValue/UIView+BadgeValue.h
+++ UIView + BadgeValue/UIView + BadgeValue/UIView+BadgeValue.h
@@ -1,10 +1,12 @@
-//
-//  UIView+BadgeValue.h
-//  OC_Demo
-//
-//  Created by clarence on 16/11/1.
-//  Copyright © 2016年 clarence. All rights reserved.
-//
+/*
+ * author 孔凡列
+ *
+ * gitHub https://github.com/gitkong
+ * cocoaChina http://code.cocoachina.com/user/
+ * 简书 http://www.jianshu.com/users/fe5700cfb223/latest_articles
+ * QQ 279761135
+ * 喜欢就给个like 和 star 喔~
+ */
 
 #import <UIKit/UIKit.h>
 
